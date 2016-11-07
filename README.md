b48d688e1a070523d8711a04de08483588b85684
Job Snijders <job@instituut.net>
2016-11-07 15:55:40 +0100

    wrap up package

---
+++ README.md
@@ -0,0 +1,8 @@
+rtrsub - RTR Substitution
+=========================
+
+```
+Vurt:rtrsub job$ python rtrsub/cli.py --afi ipv4 -t template-examples/bird.j2 -o example-output.conf
+Vurt:rtrsub job$
+```
+
---
+++ setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# Copyright (C) 2016 Job Snijders <job@instituut.net>
+#
+# This file is part of rtrsub
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF  THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import rtrsub
+version = rtrsub.__version__
+
+import codecs
+import os
+import sys
+
+from setuptools import setup, find_packages
+from os.path import abspath, dirname, join
+
+here = abspath(dirname(__file__))
+
+with codecs.open(join(here, 'README.md'), encoding='utf-8') as f:
+    README = f.read()
+
+if sys.argv[-1] == 'publish':
+    os.system('python2.7 setup.py sdist upload')
+    print("You probably want to also tag the version now:")
+    print(("  git tag -a %s -m 'version %s'" % (version, version)))
+    print("  git push --tags")
+    sys.exit()
+
+
+def get_data_files():
+    man_path = '/usr/share/man/man7'
+    if os.getenv('TRAVIS_BUILD_ID'):
+        print("not installing manpage in travis environment")
+    elif os.path.exists(man_path):
+        files = [(man_path, ['rtrsub/rtrsub.7'])]
+        return files
+
+setup(
+    name='rtrsub',
+    version=version,
+    maintainer="Job Snijders",
+    maintainer_email='job@instituut.net',
+    url='https://github.com/job/rtrsub',
+    description='RTR Substitution',
+    long_description=README,
+    license='BSD 2-Clause',
+    keywords='rpki prefix routing networking',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    entry_points={'console_scripts': ['rtrsub = rtrsub.cli:main']},
+    data_files=get_data_files()
+)
