b2d535cdbe570f734cf6c4cb2c7496768f5d5f2a
Job Snijders <job@instituut.net>
2018-01-07 15:07:33 +0000

    This is python3

--- rtrsub/rtrsub.py
+++ rtrsub/rtrsub.py
@@ -27,8 +27,6 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from __future__ import print_function
-
 from collections import OrderedDict
 from ipaddress import ip_network
 from operator import itemgetter
